b224cc312080414ee9cc6fe460bd316a39f74fd3
farzamali <gh.alifarzam@gmail.com>
2023-06-06 22:06:30 -0400

    new

--- src/test/java/api/data/GenerateData.java
+++ src/test/java/api/data/GenerateData.java
@@ -1,6 +1,7 @@
 package api.data;
 
-import com.oracle.truffle.js.builtins.math.MathBuiltins.Math;
+import java.util.Random;
+
 
 public class GenerateData {
 	
@@ -12,8 +13,38 @@ public class GenerateData {
 		return autoEmail;
 		
 	}
-	
-	
+
+	//create a method that return random phone number 10 digit
+			public static String getPhoneNumber() {
+				String phoneNumber = "9";
+				for(int i = 0; i < 9; i++) {
+					phoneNumber += (int)(Math.random() * 10);
+				}
+				return phoneNumber;
+			}
+			
+	//create a method that return random license plate 
+			public static String getLicensePlate() {
+				Random random = new Random();
+				StringBuilder sb = new StringBuilder();
+				String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				int count = 4;
+				for(int i = 0; i < count; i++) {
+					int randomIndex = random.nextInt(letters.length());
+					char randomChar = letters.charAt(randomIndex);
+					sb.append(randomChar);
+				}
+				for(int i = 0; i < 4; i++) {
+				int randomDigit = (int) (Math.random() * 10);
+				sb.append(randomDigit);
+				
+			}
+				return sb.toString();
 	
 
+}
+			public static void main(String[] args) {
+				System.out.println(getPhoneNumber());
+				System.out.println(getLicensePlate()); 
+			}
 }
